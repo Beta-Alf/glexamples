e4acbe8d37e8b62fb626ed0dcbd3b5b35337f827
Flonk <florian.bonn@gmail.com>
2015-05-17 00:19:10 +0200

    Copied the EmptyExample to create AnimationExample

---
+++ source/animationexample/plugin.cpp
@@ -0,0 +1,15 @@
+#include <gloperate/plugin/plugin_api.h>
+
+#include "AnimationExample.h"
+
+#include <glexamples-version.h>
+
+GLOPERATE_PLUGIN_LIBRARY
+
+    GLOPERATE_PLUGIN(AnimationExample
+    , "AnimationExample"
+    , "An Example demonstrating different ways of animation"
+    , "Juliane Waack and Florian Wagner"
+    , "v1.0.0" )
+
+GLOPERATE_PLUGIN_LIBRARY_END
